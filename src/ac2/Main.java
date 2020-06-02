dad7aa0663ddbe2df331302884f178966b747d2e
eeikee <konumaeike5618@hotmail.com>
2020-06-02 11:53:21 -0300

    Jogo em funcionamento

--- src/ac2/Main.java
+++ src/ac2/Main.java
@@ -12,9 +12,10 @@ public class Main {
 	String atual;
 	String opt = "n";
 
-        System.out.print("Digite a quantidade de jogadores: ");
-        numPlay = input.nextInt();
-        int pontos[] = new int[numPlay];
+        do{
+            System.out.print("Digite a quantidade de jogadores: ");
+            numPlay = input.nextInt();
+        }while(numPlay < 2);
         String nome[] = new String[numPlay];
         System.out.println("Digite o nome dos jogadores: ");
         for(int i = 0; i < numPlay;i++){
@@ -26,26 +27,39 @@ public class Main {
         System.out.println("Primeiro a jogar : " + nome[first]);
         atual = nome[first];
         while(primeiro < 3){
+            if(opt.equals("S")|| opt.equals("s")){
+                System.out.println("Lider da rodada: " + atual);
+            }
             System.out.print("Digite uma palavra: ");
             String palavra = input.next();
             System.out.print("Digite uma dica: ");
             String dica = input.next();
             int erro = 0;
-            System.out.println("Dica: " + dica);
             char secreto[] = new char[palavra.length()];
             for(int i = 0; i < palavra.length(); i++){
                 secreto[i] = '_';
             }
-            boolean fim = true;
+            boolean falta = false;
+            boolean fim = false;
             while(erro < 6){
+                System.out.println("Dica: " + dica);
                 for(int i = 0; i < palavra.length(); i++){
                     System.out.print(secreto[i]);
                 }
                 for(int i = 0; i < numPlay; i++){
                     if(atual.equals((nome[i]))){
-                        i++;
+                            i++;
                     }
-                    System.out.println("\nVez de: " + nome[i]);
+                    if(numPlay == 2 && atual.equals(nome[0])){
+                        System.out.println("\nVez de: " + nome[1]);
+                    }
+                    else if (numPlay == 2 && atual.equals(nome[1])){
+                        System.out.println("\nVez de: " + nome[0]);
+                    }
+                    else{
+                        System.out.println("\nVez de: " + nome[i]);
+                    }
+                    falta = false;
                     System.out.println("Digite uma letra: ");
                     char letra = input.next().charAt(0);
                     boolean contem = false;
@@ -59,34 +73,56 @@ public class Main {
 
                         System.out.println("Letra correta!");
                         for (int j = 0; j < palavra.length(); j++) {
-                            if ((secreto[j] == '_')) {
+                            if (secreto[j] == '_' || falta) {
                                 fim = false;
-                                break;
+                                falta = true;
                             }
-                            else{
-                                fim = true;
+                            else if(!falta){
+                                    fim = true;
                             }
                         }
                         if(fim){
-                            System.out.println(nome[i] + " acertou!");
-                            System.out.println("A palavra era " + palavra);
-                            pontos[i]++;
-                            if(pontos[i] > primeiro){
-                                primeiro = pontos[i];
+                            if(numPlay == 2 && atual.equals(nome[0])){
+                                System.out.println(nome[1] + " acertou!");
                             }
+                            else if (numPlay == 2 && atual.equals(nome[1])){
+                                System.out.println(nome[0] + " acertou!");
+                            }
+                            else{
+                                System.out.println(nome[i] + " acertou!");
+                            }
+
+                            System.out.println("A palavra era " + palavra);
+                            atual = nome[i];
+                            falta = false;
                         }
                     }
                     else{
                         System.out.println("Letra errada!");
                         erro++;
+                        falta = true;
                     }
-                    if(fim){
+                    if(falta || fim){
                         break;
                     }
                 }
+                if(!falta){
+                    System.out.println("Deseja jogar novamente? (S/n): ");
+                    opt = input.next();
+                    if(fim && opt.equals("S") || fim && opt.equals("s")){
+                        break;
+                    }
+                }
+                }
+            if(erro > 5){
+                System.out.println(atual + " venceu\nA palavra era " + palavra);
+                System.out.println("Deseja jogar novamente? (S/n): ");
+                opt = input.next();
+                if(fim && opt.equals("N") || fim && opt.equals("n")){
+                    System.out.println("Ate a proxima!");
+                    System.exit(1);
+                }
             }
         }
-
-
     }
 }
